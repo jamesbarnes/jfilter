1dde661a0a5bcd0010da84e367d357092e95f1b5
jamesbarnes <james.merritt.barnes@gmail.com>
2014-09-01 10:29:24 -0400

    added chapter select method

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 window.onload = function() { init() };
-
 var public_spreadsheet_url = config.mySheet;;
-
+var sections = [];
+var sheet = []
+var mychapter = []
 function init() {
   Tabletop.init( { key: public_spreadsheet_url,
                    callback: showInfo,
@@ -9,6 +10,24 @@ function init() {
 }
 
 function showInfo(data, tabletop) {
-  alert("Successfully processed!")
-  console.log(data);
+  sheet = data;
+  for(i=0; i<data.length; i++){
+    // console.log(data[i].element);
+    //adds parsed chapterid values to secitons array
+    sections.push(parseInt(data[i].chapterid));
+  }
+  //prints sections to console; underscore method
+  // console.log(_.uniq(sections));
+  getChapter(sheet,2)
+
 }
+
+//creates an array for a chapter
+function getChapter(data,chapter){
+  for(i=0; i<data.length; i++){
+    if (data[i].chapterid==chapter){
+      mychapter.push(data[i]);
+    }
+  }
+}
+
